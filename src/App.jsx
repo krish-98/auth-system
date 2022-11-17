74a547c71c6fa5be2e1dbfa76823ce75e9ba01a6
krish-98 <muralikrishnanm98@gmail.com>
2022-11-17 21:57:53 +0530

    initial commit with firebase and tailwind packages

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-purple-300">
+      <h1 className="text-3xl font-bold underline">Let's Test auth system</h1>
+    </div>
+  )
+}
+
+export default App
